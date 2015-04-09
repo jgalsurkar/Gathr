b1dc39936a8e9ff74cc139006b02f38f7f54ae16
Jonathan Galsurkar <jgalsurkar@gmail.com>
2015-04-09 16:22:40 -0400

    Add function for push notifications

--- app/src/main/java/com/gathr/gathr/MyGlobals.java
+++ app/src/main/java/com/gathr/gathr/MyGlobals.java
@@ -3,6 +3,11 @@ package com.gathr.gathr;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.support.v4.app.NotificationCompat;
 
 /**
  * Created by JohnnyXD1 on 4/9/2015.
@@ -34,4 +39,35 @@ public class MyGlobals {
         }
         return "";
     }
+    public void PushNotification(int uniqueID, String tickerText, String nTitle, String nText, Class<?> cls,  Context c){
+        //Lets you build new notification
+        NotificationCompat.Builder notification;
+
+        notification = new NotificationCompat.Builder(c);
+        notification.setAutoCancel(true); //This is to make the notification go away when you get to the proper intent screen
+        notification.setSmallIcon(R.mipmap.ic_launcher); //Used to set picture or logo of app for the notification
+        notification.setTicker(tickerText); //Notification Text
+        notification.setWhen(System.currentTimeMillis()); // Notification Time
+        notification.setContentTitle(nTitle);
+        notification.setContentText(nText);
+
+        //send them back to screen (in this case MainActivity
+        Intent intent = new Intent(c, cls);
+        //Gives device access to all intents in the app
+
+        PendingIntent pendingIntent = PendingIntent.getActivity(c, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+        notification.setContentIntent(pendingIntent);
+
+        //Builds the notification and issues it
+        NotificationManager nm = (NotificationManager) c.getSystemService(c.NOTIFICATION_SERVICE);
+        nm.notify(uniqueID, notification.build());
+
+    }
+
 }
+
+
+
+
+
+
